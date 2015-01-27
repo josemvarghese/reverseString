8c35b902c925d2d406d07afe45d2099354ec29fd
manjusha <manjusha@manjusha-PC>
2015-01-27 14:36:05 +0530

    Reverse a string

---
+++ Reverse/src/StringRev.java
@@ -0,0 +1,26 @@
+public class StringRev {
+
+	String s= new String("Hello my name is Jose M Varghese");
+ 
+	public int stringleg()
+	{
+		return s.length();
+	}
+	public void revstring(int p){
+		if(p<=0){
+			return;
+		}
+		else{
+			p=p-1;
+			System.out.print(s.charAt(p));
+			revstring(p);
+		}
+	}
+	public static void main(String[] args) {
+	
+	    StringRev rev= new StringRev();
+		int p=rev.stringleg();
+		rev.revstring(p); 
+	}
+
+}
